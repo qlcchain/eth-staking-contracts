d8ca511e327bc1f90918e369fcdf24cb25ff85fb
zengchen221 <931891076@qq.com>
2020-09-25 10:23:36 +0800

    feat: rename token

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -3,5 +3,5 @@ const QLCCoin = artifacts.require("QLCToken");
 const { deployProxy } = require('@openzeppelin/truffle-upgrades');
 
 module.exports = async function (deployer) {
-   await deployProxy(QLCCoin, ['QLCToken', 'qlc'], { deployer });
+   await deployProxy(QLCCoin, ['QLCToken', 'QLC'], { deployer });
 };
